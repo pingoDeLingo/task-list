5d20d78c7dc2ed1a0543882e36764ec6411441af
Robin <robin.noormets@voco.ee>
2022-09-29 10:31:39 +0300

    Add tasks into task list

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const form = document.querySelector("form")
+
 const taskInput = document.querySelector("#task")
-const taskHeading = document.querySelector("#task-title")
+form.addEventListener("submit", addTask)
 
-// submit
-form.addEventListener("submit", runEvent)
+function addTask(e){
+    console.log(taskInput.value)
+    const li = document.createElement("li")
+    li.appendChild(document.createTextNode(taskInput.value))
+    li.className = "collection-item"
+    const a = document.createElement("a")
+    a.appendChild(document.createTextNode("X"))
+    a.className = "blue-text text-darken 2 secondary-content"
+    a.setAttribute("href", "#")
+    li.appendChild(a)
+    // add to list
+    const ul = document.querySelector("ul")
+    ul.appendChild(li)
+    taskInput.value = ""
 
-// taskInput.addEventListener("keydown", runEvent)
-//taskInput.addEventListener("keypress", runEvent)
-
-/*
-taskInput.addEventListener("focus", runEvent)
-taskInput.addEventListener("blur", runEvent)
-*/
-
-taskInput.addEventListener("cut", runEvent)
-taskInput.addEventListener("paste", runEvent)
-taskInput.addEventListener("input", runEvent)
-function runEvent(event) {
-    console.log(`Event type: ${event.type}`)
-    taskHeading.innerText = event.target.value
-//    event.preventDefault()
+    e.preventDefault()
 }
