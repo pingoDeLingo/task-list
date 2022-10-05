b3af238d212b4df7b1926b31eb2ea560111b2400
Robin <robin.noormets@voco.ee>
2022-10-05 10:35:30 +0300

    As a user I can reload page and see actual data [Delivers #183462648]

--- app.js
+++ app.js
@@ -1,45 +1,92 @@
-const form = document.querySelector("form")
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
+const taskList = document.querySelector('ul')
+const delAllTasks = document.querySelector('#del-tasks')
 
-const taskInput = document.querySelector("#task")
-const taskList = document.querySelector("ul")
-const delAllTasks = document.querySelector("#del-tasks")
-
-delAllTasks.addEventListener("click", deleteAllTasks)
-taskList.addEventListener("click", deleteTask)
-
-
-
-form.addEventListener("submit", addTask)
+form.addEventListener('submit', addTask)
+taskList.addEventListener('click', deleteTask)
+delAllTasks.addEventListener('click', deleteAllTasks)
+document.addEventListener('DOMContentLoaded', getTasks)
 
 function addTask(e){
-    console.log(taskInput.value)
-    const li = document.createElement("li")
+    // create list item
+    const li = document.createElement('li')
     li.appendChild(document.createTextNode(taskInput.value))
-    li.className = "collection-item"
-    const a = document.createElement("a")
-    a.appendChild(document.createTextNode("X"))
-    a.className = "blue-text text-darken 2 secondary-content"
-    a.setAttribute("href", "#")
+    li.className = 'collection-item'
+    const a = document.createElement('a')
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken-2 secondary-content'
+    a.setAttribute('href', '#')
     li.appendChild(a)
     // add to list
-    const ul = document.querySelector("ul")
+    const ul = document.querySelector('ul')
     ul.appendChild(li)
-    taskInput.value = ""
-
+    addTaskLS(taskInput.value)
+    taskInput.value = ''
     e.preventDefault()
 }
 
 function deleteTask(e){
-    if (e.target.textContent == "X"){
-        if(confirm("Are you sure you want to delete this task?")){
+    if(e.target.textContent == 'X'){
+        if(confirm('Are you sure to delete this task?')){
             e.target.parentElement.remove()
+            deleteTaskLS(e.target.parentElement.textContent.slice(0,-1))
         }
     }
 }
 
 function deleteAllTasks(e){
-    //taskList.innerHTML = ""
+    //taskList.innerHTML = ''
     while (taskList.firstChild){
         taskList.removeChild(taskList.firstChild)
     }
+    localStorage.removeItem('tasks')
+}
+
+function addTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function deleteTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((taskLS, taskIndex) => {
+        if(taskLS === task){
+            tasks.splice(taskIndex, 1)
+        }
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function getTasks(){
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((task) => {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(task))
+        li.className = 'collection-item'
+        const a = document.createElement('a')
+        a.appendChild(document.createTextNode('X'))
+        a.className = 'blue-text text-darken-2 secondary-content'
+        a.setAttribute('href', '#')
+        li.appendChild(a)
+        // add to list
+        const ul = document.querySelector('ul')
+        ul.appendChild(li)
+    })
 }
